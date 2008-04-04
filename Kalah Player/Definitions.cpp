7bc76a826e6f0fcc47654b7c34ee372a6cc52840
Mr.Hellmaker <Mr.Hellmaker@a6ee2c79-0d4a-0410-b19e-35e742fabaac>
2008-04-04 17:14:10 +0000

    Removed file/folder

--- Kalah Player/Definitions.cpp
+++
@@ -1,9 +0,0 @@
-#include "Definitions.h"
-
-Definitions::PlayerColor Definitions::getOppositePlayer(Definitions::PlayerColor p)
-{
-	if(p == Definitions::NONE)
-		return Definitions::NONE;
-
-	return p == Definitions::WHITE ? Definitions::BLACK : Definitions::WHITE;
-};
