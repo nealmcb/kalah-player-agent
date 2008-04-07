7a6ca044e5248c88d5b736ebc0c0d56c7227d0ba
Mr.Hellmaker <Mr.Hellmaker@a6ee2c79-0d4a-0410-b19e-35e742fabaac>
2008-04-07 22:13:01 +0000

    Moved to Heuristics interface

--- KalahPlayer/OmerMarkSimpleKalahPlayer.cpp
+++
@@ -1,5 +0,0 @@
-#include "OmerMarkSimpleKalahPlayer.h"
-
-int OmerMarkSimpleKalahPlayer::utility(const KalahBoard &board) {
-	return board.getStoreContents(m_myColor) - board.getStoreContents(Definitions::getOppositePlayer(m_myColor));
-}
