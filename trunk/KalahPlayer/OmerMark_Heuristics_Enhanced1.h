c32c0d10789036235a3e60655b6fb0b84cac7875
omerlevy <omerlevy@a6ee2c79-0d4a-0410-b19e-35e742fabaac>
2008-04-18 18:15:52 +0000

    Fixed wrong direction bug when looking at opponent's houses.

--- trunk/KalahPlayer/OmerMark_Heuristics_Enhanced1.h
+++ trunk/KalahPlayer/OmerMark_Heuristics_Enhanced1.h
@@ -1,36 +1,50 @@
 #ifndef _HEURISTICS_ENHANCED1_H
 #define _HEURISTICS_ENHANCED1_H
 
+#include <iostream>
 #include "OmerMark_IHeuristics.h"
+
+using std::cout;
 
 class Heuristics_Enhanced1 :
     public IHeuristics
 {
 public:
-    Heuristics_Enhanced1() {};
+	Heuristics_Enhanced1(double _weight = 1) : weight(_weight) {};
     virtual ~Heuristics_Enhanced1() {}
 
     int getHeuristics(const KalahBoard& board, const Definitions::PlayerColor& playerColor)
     {
-	    int heuristics = 0;
-	    heuristics += countPotentialStones(board.getHousesContents(playerColor));
-	    heuristics += board.getStoreContents(playerColor);
-	    heuristics -= countPotentialStones(board.getHousesContents(Definitions::getOppositePlayer(playerColor)));
-	    heuristics -= board.getStoreContents(Definitions::getOppositePlayer(playerColor));
-	    return heuristics;
+	    double score = board.getStoreContents(playerColor) 
+					 - board.getStoreContents(Definitions::getOppositePlayer(playerColor));
+
+		vector<int> &myHouses = board.getHousesContents(playerColor);
+
+		double count = 0;
+		int i = 1;
+		for (vector<int>::reverse_iterator iterator = myHouses.rbegin(); iterator != myHouses.rend(); iterator++) {
+			if (*iterator < i) {
+			    count += *iterator;
+				count++; // Bonus point for being under the limit.
+			}
+			i++;
+	    }
+
+		vector<int> &opponentHouses = board.getHousesContents(Definitions::getOppositePlayer(playerColor));
+
+		i = 1;
+		for (vector<int>::iterator iterator = opponentHouses.begin(); iterator != opponentHouses.end(); iterator++) {
+		    if (*iterator < i) {
+			    count -= *iterator;
+				count--; // Bonus point for being under the limit.
+			}
+			i++;
+	    }
+
+		return weight*score + (1 - weight)*count;
     }
 
-protected:
-    int countPotentialStones(vector<int> houses) 
-    {
-	    int sum = 0;
-	    for (int i = 0; i < (int)houses.size(); i++) {
-		    if (houses[i] < (int)houses.size() - i) {
-			    sum += houses[i];
-		    }
-	    }
-	    return sum;
-    }
+	double weight;
 };
 
 #endif
